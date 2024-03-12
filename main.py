364419d9f034baedc74d4b37b93243e760bc229a
JonathanRGomes <161855346+JonathanRGomes@users.noreply.github.com>
2024-03-12 14:21:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 import requests
 
-# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/5fe42b50a02f78b324cb").json()
 # print(posts)
 
